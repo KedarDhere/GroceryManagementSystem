1026bf49ed631bded5f317de0841794b46c3c63b
KedarDhere <dherek@oregonstate.edu>
2023-04-12 10:35:12 -0700

    Frontend

---
+++ frontEnd/script.js
@@ -0,0 +1,5 @@
+let productListApiUrl = 'http://127.0.0.1:5000/getProducts'
+let uomListApiUrl = 'http://127.0.0.1:5000/getUom'
+let productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct'
+let productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct'
+
